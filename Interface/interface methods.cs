64dcaccb74d3f9342a0af068f186a200428af9bb
vamsivarra <121616113+vamsivarra@users.noreply.github.com>
2024-05-28 10:34:58 +0100

    first bcommit

---
+++ Interface/interface methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Interface
+{
+	public class Employee
+	{
+		public float salary = 40000;
+	}
+	public class Programmer : Employee
+	{
+		public float bonus = 10000;
+	}
+	public class QA : Programmer
+	{
+		public float extraBonus = 12000;
+	}
+	class TestInheritance
+	{
+		/*public static void Main(string[] args)
+		{
+			Programmer p1 = new Programmer();
+			Console.WriteLine("Salary: " + p1.salary);
+			Console.WriteLine("Bonus: " + p1.bonus);
+
+		}*/
+	}
+}
+
---
+++ Interface/multiple.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace interfaceexample
+{
+	public interface Idrawable
+	{
+		void test();
+		int test1();
+	}
+	public interface Idrawable1
+	{
+		void test2();
+		int test3();
+	}
+	public class Multipleinheritance : Idrawable, Idrawable1
+	{
+		public void test()
+		{
+			Console.WriteLine("i am a test method");
+		}
+		public int test1()
+		{
+			int i = 10;
+			Console.WriteLine("i a test 1 method",+i);
+			return i;
+		}
+		public void test2()
+		{
+			Console.WriteLine("i am a test 2 method");
+		}
+		public int test3()
+		{
+			int i = 10;
+			Console.WriteLine("i a test 3 method", +i);
+			return i;
+		}
+		public static void Main(string[] args)
+		{
+			Multipleinheritance m1 = new Multipleinheritance();
+			m1.test();
+			m1.test1();
+			m1.test2();
+			m1.test3();
+		}
+	}
+}
+
